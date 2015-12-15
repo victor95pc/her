f4d3fbedcaf0230d0f970ec1f85d92b7dfbc26e6
Victor Palomo de Castro <victorpalomocastro@gmail.com>
2015-12-15 15:14:52 -0200

    Update relation.rb
    clone no initialize

--- lib/her/model/relation.rb
+++ lib/her/model/relation.rb
@@ -6,7 +6,7 @@ module Her
 
       # @private
       def initialize(parent)
-        @parent = parent
+        @parent = parent.clone
         @params = {}
       end
 
@@ -44,7 +44,6 @@ module Her
       # @private
       def method_missing(method, *args, &blk)
         if @parent.respond_to? method
-        	@parent = @parent.clone
 			    @parent.call_scopes(method, args.first)
 			  else
 				  fetch.send(method, *args, &blk)
